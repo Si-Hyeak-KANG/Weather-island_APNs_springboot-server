51ad1508264c3a4aaa9b9f82d0a8e2ce495915ae
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-02-26 11:54:55 +0900

    Revert "요청 api 데이터 유효성 검증 정보 추가"
    This reverts commit 8ac5cde8877f0d1d2347ae668784e1d5de1fe868.

--- src/main/java/project/app/apns_server/modules/dto/AppInfoRequestDto.java
+++ src/main/java/project/app/apns_server/modules/dto/AppInfoRequestDto.java
@@ -1,8 +1,6 @@
 package project.app.apns_server.modules.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,24 +16,18 @@ import lombok.ToString;
 @NoArgsConstructor
 public class AppInfoRequestDto {
 
-    @NotBlank(message = "Device 토큰 미입력")
     @JsonProperty("device_token")
     private String deviceToken;
 
-    @NotBlank(message = "Push 토큰 미입력")
     @JsonProperty("push_token")
     private String pushToken;
 
-    @NotBlank(message = "APNs ID 미입력")
-    @Pattern(regexp = "\\b\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}\\b", message = "Canonical UUIDs are 32 lowercase hexadecimal digits, displayed in five groups separated by hyphens in the form 8-4-4-4-12. For example: 123e4567-e89b-12d3-a456-4266554400a0. ")
     @JsonProperty("apns_id")
     private String apnsId;
 
-    @NotBlank(message = "위도(lat) 미입력")
     @JsonProperty("lat")
     private double latitude;
 
-    @NotBlank(message = "경도(lon) 미입력")
     @JsonProperty("lon")
     private double longitude;
 }
