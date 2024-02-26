7d2202601bc1cfcf02e8f00f5bffcbfa427abcb6
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-02-26 11:56:06 +0900

    코드 포맷 수정

--- src/main/java/project/app/apns_server/modules/dto/AppInfoRequestDto.java
+++ src/main/java/project/app/apns_server/modules/dto/AppInfoRequestDto.java
@@ -27,7 +27,8 @@ public class AppInfoRequestDto {
     private String pushToken;
 
     @NotBlank(message = "APNs ID 미입력")
-    @Pattern(regexp = "\\b\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}\\b", message = "Canonical UUIDs are 32 lowercase hexadecimal digits, displayed in five groups separated by hyphens in the form 8-4-4-4-12. For example: 123e4567-e89b-12d3-a456-4266554400a0. ")
+    @Pattern(regexp = "\\b\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}\\b",
+            message = "Canonical UUIDs are 32 lowercase hexadecimal digits, displayed in five groups separated by hyphens in the form 8-4-4-4-12. For example: 123e4567-e89b-12d3-a456-4266554400a0. ")
     @JsonProperty("apns_id")
     private String apnsId;
 
